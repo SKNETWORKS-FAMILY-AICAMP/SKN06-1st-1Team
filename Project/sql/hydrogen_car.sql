f6bb1e7af277dcea750bb1d9cf5a11092c93a447
zl-zone <clehflek@gmail.com>
2024-10-13 13:43:06 +0900

    Add files via upload

---
+++ Project/sql/hydrogen_car.sql
@@ -0,0 +1,12 @@
+CREATE TABLE hydrogen_car (
+    증가량 VARCHAR(255),
+    계 VARCHAR(255),
+    년도 VARCHAR(255)
+);
+
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES (NULL, '893.0', '2018.0');
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES ('4190.0', '5083.0', '2019.0');
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES ('5823.0', '10906.0', '2020.0');
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES ('8498.0', '19404.0', '2021.0');
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES ('10219.0', '29623.0', '2022.0');
+INSERT INTO hydrogen_car (증가량, 계, 년도) VALUES ('4635.0', '34258.0', '2023.0');
