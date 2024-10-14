4edb3b70cb5f5312a25223f428b380a20bc97af6
WonJae0410 <nwj5664@naver.com>
2024-10-14 18:14:56 +0900

    Add files via upload

---
+++ Project/Project/sql/hydrogen_registration_region.sql
@@ -0,0 +1,24 @@
+use Project;
+
+CREATE TABLE `hydrogen_registration_region` (
+    `지역` VARCHAR(255),
+    `차량 수` FLOAT
+);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('서울', 3270);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('부산', 2115);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('대구', 673);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('인천', 2324);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('광주', 1253);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('대전', 1812);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('울산', 2951);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('세종', 500);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('경기', 7970);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('강원', 2731);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('충북', 2235);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('충남', 1707);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('전북', 2556);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('전남', 1177);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('경북', 508);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('경남', 2955);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('제주', 50);
+INSERT INTO `hydrogen_registration_region` (`지역`, `차량 수`) VALUES ('계', 36787);
